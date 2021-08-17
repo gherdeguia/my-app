8c4938c67d1291ee7b7194dd0ac58269685b3045
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-17 20:15:16 +0800

    [ReactRedux][GG] feat : created todosReducer

---
+++ src/features/todos/reducers/todosReducer.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todosSlice = createSlice({
+    name: "todos",
+    initialState: {},
+    reducers: {
+        
+    },
+});
+
+
+
+
+export default todosSlice.reducer;
