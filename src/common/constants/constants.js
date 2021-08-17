98a867d6670e60c5f20051ee9d8fd064241a2764
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-17 20:18:04 +0800

    [ReactRedux][GG] refactor : refactoring constants for data correction

---
+++ src/common/constants/constants.js
@@ -0,0 +1,31 @@
+export const initialTodoList = 
+[
+    {
+        // id: "cc53dc26-61b0-406b-99dd-b8825dd2ceec",
+        id: 1,
+        text: "todo example",
+        done: false
+    },
+    {
+        // id: "cc53dc26-61b0-406b-99dd-b8825dd2ceec",
+        id: 2,
+        text: "todo example 2",
+        done: false
+    }
+    
+]
+
+
+// ids:["1","2"],
+//     entities: {
+//         1: {
+//             id:"1",
+//             text: "texting code something something",
+//             done: false
+//         },
+//         2: {
+//             id:"2",
+//             text: "second testing testing",
+//             done: true
+//         },
+//     },
