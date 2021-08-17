94d45efc22be36524ae3f67cc6d99a998b4c330e
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-17 20:14:21 +0800

    [ReactRedux][GG] feat : created todosSlice Reduced

---
+++ src/features/todos/reducers/todosSlice.js
@@ -0,0 +1,60 @@
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+import { initialTodoList } from "../../../common/constants/constants";
+import {v4 as uuid} from "uuid";
+const todosAdapter = createEntityAdapter();
+// const initialState = todosAdapter.getInitialState({
+//     ids:["1","2"],
+//     entities: {
+//         1: {
+//             id:"1",
+//             text: "texting code something something",
+//             done: false
+//         },
+//         2: {
+//             id:"2",
+//             text: "second testing testing",
+//             done: true
+//         },
+//     },
+    
+// });
+
+const initialState = todosAdapter.getInitialState({
+    ids:[initialTodoList.id],
+    entities: {[initialTodoList.id]: initialTodoList}
+});
+
+//create slices list name todos
+const todosSlice = createSlice({
+    name : "todos",
+    initialState : initialState,
+    reducers : {
+        AddTodo(state, action) {
+            // console.log("Add : ", action)
+            todosAdapter.addOne(state, {
+                id : uuid(),
+                text: action.payload,
+                done: false,
+            });
+            return state;
+        },
+        ToggleTodo(state, action) {
+            const todo = state.entities[action.payload];
+            todo.done = !todo.done;
+            // console.log("Add : ", action)
+            // todosAdapter.updateOne(state, {
+            //     id : uuid,
+            //     text: action.payload,
+            //     done: false,
+            // });
+            // return state;
+        },
+    },
+});
+
+export const {AddTodo, ToggleTodo} = todosSlice.actions;
+
+export const {selectIds : selectTodoIds, selectById: selectTodoById} = 
+todosAdapter.getSelectors((state) => state.todoList);
+
+export default todosSlice.reducer;
