15694e5eb2607c4c973375387177da3752c8f413
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-17 20:16:20 +0800

    [ReactRedux][GG] feat : created utils.js

---
+++ src/common/utils/utils.js
@@ -0,0 +1,9 @@
+export function getAllTodoIds(todos){
+    return todos.map((todo) => todo.id);
+    
+}
+
+export function getTodoById(todos, id){
+    return todos.find((todo) => todo.id === id);
+}
+
